21df644965a150f2dd0ef00d689472b23c6630cb
sduos4029-CODE <sduos4029@gmail.com>
2025-10-10 21:50:29 +0530

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -237,4 +237,5 @@ def get_progress(download_id):
 # Main entry
 # ----------------------------
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
